d3e7a9c783059cc0ec99d4d2e69bf2b01a70c825
thaisaam <142519036+thaisaam@users.noreply.github.com>
2024-10-07 08:38:02 -0300

    Add files via upload

---
+++ aula 1/Cadastro.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cadastro{
+  private List<Cliente> clientes;
+
+  public Cadastro(){
+    this.clientes = new ArrayList<Cliente>();
+  }
+  public void adicionarCliente(Cliente cliente){
+    clientes.add(cliente);
+    System.out.println("Cliente adicionado com sucesso!");
+  }
+  public void listarClientes(){
+    if (clientes.isEmpty()){
+      System.out.println("Nenhum clientes registrados no momento");
+      return;
+      
+    }
+    System.out.println("Listando clientes...");
+    for(Cliente cliente : clientes){
+      System.out.println(cliente);
+    }
+  }
+}
